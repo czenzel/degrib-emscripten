52ab39320939b71e62debc1f83ed118ddf22a9e9
Christopher Zenzel <christopher@arendellekingdom.com>
2024-01-07 01:11:04 -0500

    Get final binaries ready

--- degrib-emscripten/app/src/config-emcc.sh
+++ degrib-emscripten/app/src/config-emcc.sh
@@ -8,3 +8,7 @@ emconfigure ./configure --host none
 emmake make
 
 cp degrib/degrib /build/degrib.js
+cp degrib/degrib.bc /build/degrib.bc
+
+cp degrib/degrib_DP /build/degrib_DP.js
+cp degrib/degrib_DP.bc /build/degrib_DP.bc
