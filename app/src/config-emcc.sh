46316d5b0a7e1459df1c827f66957bd350d4dd65
Christopher Zenzel <christopher@arendellekingdom.com>
2024-01-07 00:23:11 -0500

    Remove additional commented code

--- app/src/config-emcc.sh
+++ app/src/config-emcc.sh
@@ -5,7 +5,3 @@ export AR=emar
 
 emconfigure ./configure --host none
 emmake make
-
-#configure CFLAGS="-O3" TCL_PREFIX=/usr/local/tcltk
-#configure CFLAGS="-O3" TCL_PREFIX=/usr/local/tcltk --with-halo
-#configure CFLAGS="-O3" --with-halo CC=gcc34
